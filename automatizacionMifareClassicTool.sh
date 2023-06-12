ae88b09c82a79d2d9309095a1eb41f59fe22e965
Ainhoa <akinbequin@gmail.com>
2023-06-12 22:53:39 +0200

    04-Automatizacion para Mifare anadida

---
+++ automatizacionMifareClassicTool.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ../../Descargas/pmd-bin-7.0.0-rc2/bin/
+
+./pmd check -d ../../../aplicacion/gpi2/MifareClassicTool-master/classicTool/ -R rulesets/java/quickstart.xml -f text  >  ../../../aplicacion/gpi2/pmd_MifareClassicTool
+
+cd ../../../aplicacion/gpi2/MifareClassicTool-master/classicTool/
+
+./gradlew assembleDebug
+
+cp /app/build/outputs/apk/debug/apk-debug.apk ../../  
+
